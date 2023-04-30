33604b92b5ae5177e2e67662bcd4cb4fbe6207af
Carl Kamholtz <carl.kamholtz@gmail.com>
2023-04-30 21:26:33 +1000

    wip: getting data out of packet

--- r24avd1.cpp
+++ r24avd1.cpp
@@ -1,4 +1,5 @@
 #include "r24avd1.h"
+#include "crc.h"
 
 namespace esphome {
 namespace r24avd1 {
@@ -26,29 +27,40 @@ int R24AVD1Component::readline_(int readch, uint8_t *buffer, int len, int initia
 
   if (readch >= 0) {
     // ESP_LOGD(TAG, "%c", readch);
-    if (pos < len - 1) {
+    if (pos < len - 1 && ((pos == START_IDX && readch == START_VAL) || pos != 0)) {
       buffer[pos++] = readch;
       buffer[pos] = 0;
     } else {
       pos = 0;
     }
-    if (pos >= 4) {
-      if (buffer[pos - 4] == 0xF8 && buffer[pos - 3] == 0xF7 && buffer[pos - 2] == 0xF6 && buffer[pos - 1] == 0xF5) {
-        // ESP_LOGV(TAG, "Will handle Periodic Data");
-        // this->handle_periodic_data_(buffer, pos);
-        pos = 0;  // Reset position index ready for next time
 
-        // if (this->sensor1_sensor_ != nullptr) {
-        //   int new_sensor1 = 0x01;
-        //   if (this->sensor1_sensor_->get_state() != new_sensor1)
-        //     this->sensor1_sensor_->publish_state(new_sensor1);
-        // }
+    if (pos > DATA_START_IDX) {
+      const uint16_t pkt_len = (buffer[ADDRESS_H_IDX] << 8) | buffer[ADDRESS_L_IDX];
+      const uint16_t pkt_len_incl_start = pkt_len + 1;
+      if (pos > (pkt_len_incl_start - 1)) {
+        const uint8_t function_code = buffer[FUNCTION_CODE_IDX];
+        const uint8_t address_code_1 = buffer[ADDRESS_CODE_1_IDX];
+        const uint8_t address_code_2 = buffer[ADDRESS_CODE_2_IDX];
+        const uint8_t crc_l = buffer[pkt_len_incl_start - 2];
+        const uint8_t crc_h = buffer[pkt_len - 1];
+        const uint8_t data_len = (pkt_len_incl_start - 1) - address_code_2;
 
-      } else if (buffer[pos - 4] == 0x04 && buffer[pos - 3] == 0x03 && buffer[pos - 2] == 0x02 &&
-                 buffer[pos - 1] == 0x01) {
-        // ESP_LOGV(TAG, "Will handle ACK Data");
-        // this->handle_ack_data_(buffer, pos);
-        pos = 0;  // Reset position index ready for next time
+        float_data float_data_union;
+        for (uint8_t data_ii = 0; data_ii < data_len; data_ii++) {
+          float_data_union.data[data_ii] = buffer[DATA_START_IDX + data_ii];
+        }
+
+        ESP_LOGD(TAG, "function_code: %X", function_code);
+        ESP_LOGD(TAG, "address_code_1: %X", address_code_1);
+        ESP_LOGD(TAG, "address_code_2: %X", address_code_2);
+        ESP_LOGD(TAG, "crc_l: %X", crc_l);
+        ESP_LOGD(TAG, "crc_h: %X", crc_h);
+        ESP_LOGD(TAG, "data_len: %d", data_len);
+        ESP_LOGD(TAG, "float_data_union.data: %X %X %X %X", float_data_union.data[0], float_data_union.data[1], float_data_union.data[2], float_data_union.data[3]);
+        ESP_LOGD(TAG, "float_data_union: %f", float_data_union.f);
+
+        // finished parsing
+        pos = 0;
       }
     }
   }
--- r24avd1.h
+++ r24avd1.h
@@ -7,6 +7,23 @@
 namespace esphome {
 namespace r24avd1 {
 
+const uint8_t START_VAL = 0x55;
+const uint8_t START_IDX = 0;
+const uint8_t ADDRESS_START_IDX = 1;
+const uint8_t ADDRESS_L_IDX = 1;
+const uint8_t ADDRESS_H_IDX = 2;
+const uint8_t ADDRESS_LEN = 2;
+const uint8_t FUNCTION_CODE_IDX = 3;
+const uint8_t ADDRESS_CODE_1_IDX = 4;
+const uint8_t ADDRESS_CODE_2_IDX = 5;
+const uint8_t DATA_START_IDX = 5;
+const uint8_t DATA_MAX_LEN = 4;
+const uint8_t PACKET_MAX_LEN = 12;
+
+using float_data = union {
+  uint8_t data[4];
+  float f;
+};
 
 class R24AVD1Component : public Component, public uart::UARTDevice {
 
