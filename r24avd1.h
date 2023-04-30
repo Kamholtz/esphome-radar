5afe47d89a31d090b523831a00055ae043aad6e1
Carl Kamholtz <carl.kamholtz@gmail.com>
2023-04-30 17:48:16 +1000

    wip: errors on compile

--- r24avd1.h
+++ r24avd1.h
@@ -1,5 +1,6 @@
 #include "esphome/core/component.h"
 #include "esphome/components/uart/uart.h"
+#include "esphome/components/sensor/sensor.h"
 #include "esphome/core/log.h"
 
 namespace esphome {
@@ -8,13 +9,16 @@ namespace r24avd1 {
 
 class R24AVD1Component : public Component, public uart::UARTDevice {
 
-
  public:
+  R24AVD1Component(uart::UARTComponent *parent) : uart::UARTDevice(parent) {}
+  sensor::Sensor *sensor1 = new sensor::Sensor();
   void setup() override;
-  void dump_config() override;
+  // void dump_config() override;
   void loop() override;
+ protected:
+  void readline_(int readch, uint8_t *buffer, int len);
 
-}
+};
 
 }  // namespace r24avd1
 }  // namespace esphome
