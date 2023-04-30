3055578dfa1782b1f81e42c93632955ca988c6ee
Carl Kamholtz <carl.kamholtz@gmail.com>
2023-04-30 19:00:39 +1000

    feat: accept uart device

--- r24avd1.cpp
+++ r24avd1.cpp
@@ -6,15 +6,55 @@ namespace r24avd1 {
 static const char *const TAG = "r24avd1";
 
 void R24AVD1Component::setup() {
-  ESP_LOGD("a", "b");
+  // nothing to do
 }
 void R24AVD1Component::dump_config() {
-  ESP_LOGD("a", "b");
+  // not implemented
 }
 void R24AVD1Component::loop() {
+  const int max_line_length = 80;
+  static uint8_t buffer[max_line_length];
 
-  ESP_LOGD("a", "b");
+  while (available()) {
+    this->readline_(read(), buffer, max_line_length);
+  }
 }
+
+void R24AVD1Component::readline_(int readch, uint8_t *buffer, int len) {
+  static int pos = 0;
+
+  if (readch >= 0) {
+    ESP_LOGD(TAG, "%c", readch);
+    if (pos < len - 1) {
+      buffer[pos++] = readch;
+      buffer[pos] = 0;
+    } else {
+      pos = 0;
+    }
+    if (pos >= 4) {
+      if (buffer[pos - 4] == 0xF8 && buffer[pos - 3] == 0xF7 && buffer[pos - 2] == 0xF6 && buffer[pos - 1] == 0xF5) {
+        // ESP_LOGV(TAG, "Will handle Periodic Data");
+        // this->handle_periodic_data_(buffer, pos);
+        pos = 0;  // Reset position index ready for next time
+
+        // if (this->sensor1_sensor_ != nullptr) {
+        //   int new_sensor1 = 0x01;
+        //   if (this->sensor1_sensor_->get_state() != new_sensor1)
+        //     this->sensor1_sensor_->publish_state(new_sensor1);
+        // }
+
+      } else if (buffer[pos - 4] == 0x04 && buffer[pos - 3] == 0x03 && buffer[pos - 2] == 0x02 &&
+                 buffer[pos - 1] == 0x01) {
+        // ESP_LOGV(TAG, "Will handle ACK Data");
+        // this->handle_ack_data_(buffer, pos);
+        pos = 0;  // Reset position index ready for next time
+      }
+    }
+  }
+}
+
+
+
 
 
 }  // namespace r24avd1
--- r24avd1.h
+++ r24avd1.h
@@ -11,12 +11,15 @@ namespace r24avd1 {
 class R24AVD1Component : public Component, public uart::UARTDevice {
 
   public:
+    R24AVD1Component(uart::UARTComponent *parent) : uart::UARTDevice(parent) {}
     sensor::Sensor *sensor1 = new sensor::Sensor();
     void setup() override;
     void dump_config() override;
     void loop() override;
 
 
+  protected:
+    void readline_(int readch, uint8_t *buffer, int len); 
 
 };
 }  // namespace r24avd1
