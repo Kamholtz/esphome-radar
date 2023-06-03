0d9a8d5f93e41581b19af0e7f73902291bc69b44
Carl Kamholtz <carl.kamholtz@gmail.com>
2023-06-03 14:11:04 +1000

    feat: link another yaml file

--- setup.ps1
+++ setup.ps1
@@ -1,4 +1,5 @@
 # run this in admin powershell
 mkdir ../r24avd1-test
 New-Item -ItemType SymbolicLink -Path "../r24avd1-test/esphome-external.yaml" -Target ".\esphome-web-ac358c-external.yaml"
-New-Item -ItemType SymbolicLink -Path "../r24avd1-test/esphome-external-downstairs.yaml" -Target ".\esphome-web-abf2ec-external.yaml"
+New-Item -ItemType SymbolicLink -Path "../r24avd1-test/esphome-external-balcony.yaml" -Target ".\esphome-web-abf2ec-external.yaml"
+New-Item -ItemType SymbolicLink -Path "../r24avd1-test/esphome-external-downstairs.yaml" -Target ".\esphome-web-abf408-external.yaml"
